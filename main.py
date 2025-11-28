78746b760f59ac0eadc6ae1721191a707d097617
Настя <nastavaganova79@gmail.com>
2025-11-29 00:21:12 +0400

    added version_2

---
+++ main.py
@@ -0,0 +1,34 @@
+print("введите два числа")
+a = int(input())
+b = int(input())
+print("выберите действие")
+c = str(input())
+if(c == "+"):
+    s = a + b
+    print(s)
+elif(c == "-"):
+    s = a - b
+    print(s)
+elif(c == "*"):
+    s = a * b
+    print(s)
+elif(c == "//"):
+    s = a // b
+    print(s)print("введите два числа")
+a = int(input())
+b = int(input())
+print("выберите действие")
+c = str(input())
+if(c == "+"):
+    s = a + b
+    print(s)
+elif(c == "-"):
+    s = a - b
+    print(s)
+elif(c == "*"):
+    s = a * b
+    print(s)
+elif(c == "//"):
+    s = a // b
+    print(s)
+    #калькулятор
